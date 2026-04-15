88d141e95f55a7f3bc93d96b670966c0d7693b62
Amor789 <153350889+Amor789@users.noreply.github.com>
2026-04-16 02:24:50 +0400

    Update ДЗ: Создание простого калькулятора.py

--- dz21.py/ДЗ: Создание простого калькулятора.py
+++ dz21.py/ДЗ: Создание простого калькулятора.py
@@ -1,55 +1,50 @@
 print("Добро пожаловать в кальекулятор!")
 
 while True:
-    a = float(input("Введите первое число: "))
-    b = float(input("Введите второе число: "))
+   try:
+       a = float(input("Введите первое число: "))
+       b = float(input("Введите второе число: "))
 
-    message = '''
-        Выберете математическую операцию:
 
-        + : Сложение
-        - : Вычитание
-        / : Деление
-        * : Умножение
+       message = '''
+              Выберете математическую операцию:
 
-        Ваш выбор:
-        '''
-    operation = input(message)
+              + : Сложение
+              - : Вычитание
+              / : Деление
+              * : Умножение
 
-    try:
-        if operation == '+':
-            print('Сложение')
-            result = a + b
-        elif operation == '-':
+              Ваш выбор:
+              '''
+       operation = input(message)
+       if operation == '+':
+           print('Сложение')
+           result = a + b
+       elif operation == '-':
             print('Вычитание')
             result = a - b
-        elif operation == '/':
+       elif operation == '/':
             print('Деление')
-            try:
-                result = a / b
-            except ZeroDivisionError:
-                print("Камон, на ноль делить нельзя!")
-
-        elif operation == '*':
+            result = a / b
+       elif operation == '*':
             print('Умножение')
             result = a * b
-        else:
+       else:
             print('Неизвестная операция')
 
-        print("Результат:", result)
-
-
-    except ValueError:
-        print("Ошибка: введено не число!")
-    else:
-        print("Операция выполнена успешно.")
-    b = input("Продолжим?\n" "'Y' это да, 'N' это нет\n")
-    if b == "Y" or b == "y":
-        True
-    elif b == "n" or b == "N":
-        print("Программа завершина) Чао")
-        break
-
+       print("Результат:", result)
+       
+   except ZeroDivisionError:
+       print("Камон, на ноль делить нельзя!")
+   except ValueError:
+       print("Ошибка: введено не число!")
+       
+   сhoice = input("Продолжим?\n" "'Y' это значит да ")
+   if сhoice.lower() == "y":
+       True
+   else:
+       print("Программа завершина) Чао")
+       break
 
 
 
