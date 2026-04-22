71983531cb187e28a2902ea1aabdd7fbe8649362
Amor789 <153350889+Amor789@users.noreply.github.com>
2026-04-22 23:25:26 +0400

    Update ДЗ: Переводчик чисел в текст.py

--- dz21.py/ДЗ: Переводчик чисел в текст.py
+++ dz21.py/ДЗ: Переводчик чисел в текст.py
@@ -1,18 +1,25 @@
 try:
     number = int(input("Введите число в диапазоне от 1 до 5: "))
-    
-    if number == 1:
-      print("Соответствующее слово: One")
-    elif number == 2:
-      print("Соответствующее слово: Two")
-    elif number == 3:
-      print("Соответствующее слово: Three")
-    elif number == 4:
-      print("Соответствующее слово: Four")
-    elif number == 5:
-      print("Соответствующее слово: Five")
-    else:
-      print("Вы не попали в диапазон от 1 до 5, попробуйте позже")
-        
 except ValueError:
-       print("Ошибка: введено не число!")
+    print("Ошибка: введено не число!")
+
+number_dict ={
+    "1" : " One",
+    "2" : " Two",
+    "3" : " Three",
+    "4" : " Four",
+    "5" : " Five"
+}
+
+if number == 1:
+    print(f"Соответствующее слово:{number_dict["1"]}")
+elif number == 2:
+    print(f"Соответствующее слово:{number_dict["2"]}")
+elif number == 3:
+    print(f"Соответствующее слово:{number_dict["3"]}")
+elif number == 4:
+    print(f"Соответствующее слово:{number_dict["4"]}")
+elif number == 5:
+    print(f"Соответствующее слово:{number_dict["5"]}")
+else:
+    print("Вы не попали в диапазон от 1 до 5, попробуйте позже")
