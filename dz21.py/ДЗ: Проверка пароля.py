8ac953878a89f240a8b6f687d68d468ec8d7fbce
Amor789 <153350889+Amor789@users.noreply.github.com>
2026-05-01 23:51:56 +0400

    Update ДЗ: Проверка пароля.py

--- dz21.py/ДЗ: Проверка пароля.py
+++ dz21.py/ДЗ: Проверка пароля.py
@@ -1,7 +1,7 @@
 password = "ama_1234_10"
 
 while 1 == 1:
-    password_2 = str(input("Введите пароль: "))
+    password_2 = (input("Введите пароль: "))
 
     if password_2 == password:
         print("Верный пароль!")
