9b231070e9cbf2d719f2c2ec0ea41d2885142d75
Amor789 <153350889+Amor789@users.noreply.github.com>
2026-05-06 23:58:55 +0400

    Update ДЗ: Анализ текста.py

--- dz21.py/ДЗ: Анализ текста.py
+++ dz21.py/ДЗ: Анализ текста.py
@@ -1,16 +1,14 @@
 import string
 
 #колличество слов в тексте
-def number_of_words_in_the_text(text):
-    cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
-    words = [word for word in cleaned_text.split() if word]
+def get_number_of_words_in_the_text(text):
+    words = [word for word in text.split() if word]
     return len(words)
 
 
 #Самое длинное слово в тексте
-def the_longest_word_in_the_text(text):
-    cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
-    words = cleaned_text.split()
+def get_the_longest_word_in_the_text(text):
+    words = text.split()
 
     if not words:
         return ""
@@ -19,19 +17,18 @@ def the_longest_word_in_the_text(text):
 
 
 #Количество гласных в тексте
-def number_of_vowels_in_the_text(text):
-    slov = set('аеёиоуыэюя')
+def get_the_number_of_vowels_in_the_text(text):
+    vowels = set('аеёиоуыэюя')
     count = 0
-    for i in text.lower():
-        if i in slov:
+    for symbol in text.lower():
+        if symbol in vowels:
             count +=1
     return count
 
 
 #Слова и их повторение
-def words_and_their_repetition(text):
-    cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
-    words = cleaned_text.lower().split()
+def get_the_words_and_their_repetition(text):
+    words = text.lower().split()
     word_count = {}
 
 
@@ -44,10 +41,11 @@ def words_and_their_repetition(text):
     return word_count
 
 
-user_text = input("Введите текст для анализа: ")
 
-print(f"Количество слов в тексте {number_of_words_in_the_text(user_text)}")
-print(f"Самое длинное слово в тексте {the_longest_word_in_the_text(user_text)}")
-print(f"Количество гласных в тексте {number_of_vowels_in_the_text(user_text)}")
-print(f"Слова и их повторение {words_and_their_repetition(user_text)}")(poem)}")
-print(f"Слова и их повторение {double_t(poem)}")
+user_text = input("Введите текст для анализа: ")
+processed_text = user_text.translate(str.maketrans('', '', string.punctuation))
+
+print(f"Количество слов в тексте {get_number_of_words_in_the_text(processed_text)}")
+print(f"Самое длинное слово в тексте {get_the_longest_word_in_the_text(processed_text)}")
+print(f"Количество гласных в тексте {get_the_number_of_vowels_in_the_text(processed_text)}")
+print(f"Слова и их повторение {get_the_words_and_their_repetition(processed_text)}")
