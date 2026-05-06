f710f6ae15a0dda7530f9cc520681f23d56c41c2
Amor789 <153350889+Amor789@users.noreply.github.com>
2026-05-07 00:27:06 +0400

    Update ДЗ: Применение условий.py

--- dz21.py/ДЗ: Применение условий.py
+++ dz21.py/ДЗ: Применение условий.py
@@ -1,23 +1,22 @@
-count = 0
-count2 = []
+sum_of_even_numbers = 0
+entered_numbers = []
+number = 0
 
 data = [i for i in range(0, 101) if i % 2 == 0]
 for i in data:
-    count += i
+    sum_of_even_numbers += i
 
-data2 = [(lambda i: i*i)(i) for i in range(0, 10) if i % 2 != 0]
+the_square_of_odd_numbers = [ i * i for i in range(1, 10,2)]
 
 print("Вводите числа. Для завершения введите отрицательное число.")
 
-while True:
+while number >= 0:
     try:
         number = int(input("Введите число: "))
-        if number < 0:
-            break
-        count2.append(number)
+        entered_numbers.append(number)
     except ValueError:
         print("Ошибка! Пожалуйста, введите корректное число.")
 
-print(f"Сумма всех четных чисел от 1 до 100: {count}")
-print(f"Квадраты всех нечетных чисел от 1 до 10: {data2}")
-print(f"Количество введённых неотрицательных чисел: {count2}")
+print(f"Сумма всех четных чисел от 1 до 100: {sum_of_even_numbers}")
+print(f"Квадраты всех нечетных чисел от 1 до 10: {the_square_of_odd_numbers}")
+print(f"Количество введённых чисел: {entered_numbers}")
