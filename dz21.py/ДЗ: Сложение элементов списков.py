3a2efb13566de15ca45984ed6a7c34618bb1f2fe
Amor789 <153350889+Amor789@users.noreply.github.com>
2026-05-08 20:47:10 +0400

    Update ДЗ: Сложение элементов списков.py

--- dz21.py/ДЗ: Сложение элементов списков.py
+++ dz21.py/ДЗ: Сложение элементов списков.py
@@ -1,11 +1,18 @@
 import random
 
-row_size = int(input("Введи размер строк: "))
+try:
+    row_size = int(input("Введи размер строк: "))
+    first_list = [random.randint(1, 100) for i in range(row_size)]
+    second_list = [random.randint(1, 100) for i in range(row_size)]
+    final_list = [first_list[i] + second_list[i] for i in range(row_size)]
 
-first_list = [random.randint(1, 100) for i in range(row_size)]
-second_list = [random.randint(1, 100) for i in range(row_size)]
-final_list = [first_list[i] + second_list[i] for i in range(row_size)]
-
-print(first_list)
-print(second_list)
-print(final_list)
+except ValueError:
+    print("Ошибка: Введите корректное число!")
+    
+else:
+    print(first_list)
+    print(second_list)
+    print(final_list)
+    
+finally:
+    print("Программа завершена.")
