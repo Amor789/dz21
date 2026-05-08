b079702a82ca162496f116dc04e664d8f7ca46cc
Amor789 <153350889+Amor789@users.noreply.github.com>
2026-05-08 19:41:52 +0400

    Update ДЗ: Применение условий.py

--- dz21.py/ДЗ: Применение условий.py
+++ dz21.py/ДЗ: Применение условий.py
@@ -1,22 +1,31 @@
+def to_get_the_sum_of_all_numbers(sum_of_even_numbers):
+    data = [i for i in range(0, 101) if i % 2 == 0]
+    for i in data:
+        sum_of_even_numbers += i
+    print(f"Сумма всех четных чисел от 1 до 100: {sum_of_even_numbers}")
+
+
+def get_the_square_of_odd_numbers():
+    the_square_of_odd_numbers = [i * i for i in range(1, 10, 2)]
+    print(f"Квадраты всех нечетных чисел от 1 до 10: {the_square_of_odd_numbers}")
+
+
+def get_the_number_of_entered_numbers(entered_numbers):
+    number = 0
+    while number >= 0:
+        try:
+            number = int(input("Введите число: "))
+            entered_numbers.append(number)
+        except ValueError:
+            print("Ошибка! Пожалуйста, введите корректное число.")
+    print(f"Количество введённых чисел: {entered_numbers}")
+
+
 sum_of_even_numbers = 0
 entered_numbers = []
-number = 0
 
-data = [i for i in range(0, 101) if i % 2 == 0]
-for i in data:
-    sum_of_even_numbers += i
+to_get_the_sum_of_all_numbers(sum_of_even_numbers)
+get_the_square_of_odd_numbers()
 
-the_square_of_odd_numbers = [ i * i for i in range(1, 10,2)]
-
-print("Вводите числа. Для завершения введите отрицательное число.")
-
-while number >= 0:
-    try:
-        number = int(input("Введите число: "))
-        entered_numbers.append(number)
-    except ValueError:
-        print("Ошибка! Пожалуйста, введите корректное число.")
-
-print(f"Сумма всех четных чисел от 1 до 100: {sum_of_even_numbers}")
-print(f"Квадраты всех нечетных чисел от 1 до 10: {the_square_of_odd_numbers}")
-print(f"Количество введённых чисел: {entered_numbers}")
+print("Введите числа. Для завершения введите отрицательное число.")
+get_the_number_of_entered_numbers(entered_numbers)
