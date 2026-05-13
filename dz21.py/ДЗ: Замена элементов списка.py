a134f9370785fd9f2654a55dfbe75a782270e481
Amor789 <153350889+Amor789@users.noreply.github.com>
2026-05-13 20:24:48 +0400

    Update ДЗ: Замена элементов списка.py

--- dz21.py/ДЗ: Замена элементов списка.py
+++ dz21.py/ДЗ: Замена элементов списка.py
@@ -1,5 +1,4 @@
 main_list = []
-temporary_list = [None,None]
 number = 0
 
 print("Необходимо ввести 5 слов ниже, далее я махну местами первое и последнее")
@@ -10,12 +9,6 @@ for i in range(5):
     main_list.append(word)
 print(main_list)
 
-temporary_list [0] = main_list [0]
-temporary_list [1] = main_list [4]
+main_list[4], main_list[0] = main_list[0],main_list[4]
 
-main_list.pop(0)
-main_list.pop(3)
-
-main_list.insert(0, temporary_list[1])
-main_list.append( temporary_list[0])
 print(main_list)
