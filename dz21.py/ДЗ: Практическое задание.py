f864ce02705cf4f6a3b8cfabb46b4cfd49b1c906
Amor789 <153350889+Amor789@users.noreply.github.com>
2026-05-18 22:31:31 +0400

    Update ДЗ: Практическое задание.py

--- dz21.py/ДЗ: Практическое задание.py
+++ dz21.py/ДЗ: Практическое задание.py
@@ -1,40 +1,26 @@
 #Среднее значение по студенту
 def calculate_average(grades):
-    n = 0
+    n = sum(grades)
     counter = 0
-    for element in grades:
-        n += element
+    for i in grades:
         counter += 1
     return n/counter
 
 #Находим самого слабого
 def get_the_lowers(students):
-    the_lowest_2 = students[0]
-    average_clone = calculate_average(students[0]["grades"])
-    for student in students[1:]:
-        average = calculate_average(student["grades"])
-        if  average < average_clone:
-            average_clone = average
-            the_lowest_2 = student
-    return the_lowest_2
+    return min(students,key=lambda student: calculate_average(student["grades"]))
 
 # Среднее значение по группе
 def obtaining_the_groups_average_score(students):
-    overall_grade_point_average = 0
-    for student in students:
-        average = calculate_average(student["grades"])
-        overall_grade_point_average += average
-    return overall_grade_point_average/len(students)
+    total_average = sum(calculate_average(student["grades"]) for student in students)
+    return total_average / len(students)
 
 #Общий список студентов, баллы и статус
 def request_for_student_data(students):
     for student in students:
         name = student["name"]
         average = calculate_average(student["grades"])
-        if average >= 75:
-            status = "Успешен"
-        else:
-            status = "Отстаёт"
+        status = "Успешен" if average >= 75 else "Отстаёт"
 
         print(f"Студент: {name}\nСредний балл: {average:.2f}\nСтатус: {status}\n")
 
